4a93137e5c59107fc6d08372e3430244abfc83d8
suriyasivaprakashp <psspthe.12121971@gmail.com>
2026-04-08 17:37:21 +0530

    Update 01_Hotel_Schema_Setup.sql

--- SQL/01_Hotel_Schema_Setup.sql
+++ SQL/01_Hotel_Schema_Setup.sql
@@ -1,1 +1,27 @@
+CREATE TABLE users (
+  user_id INT PRIMARY KEY,
+  name VARCHAR(50)
+);
 
+CREATE TABLE bookings (
+  booking_id INT PRIMARY KEY,
+  user_id INT,
+  room_no INT,
+  booking_date DATE,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE items (
+  item_id INT PRIMARY KEY,
+  item_name VARCHAR(50),
+  rate INT
+);
+
+CREATE TABLE booking_commercials (
+  id INT PRIMARY KEY,
+  booking_id INT,
+  item_id INT,
+  quantity INT,
+  FOREIGN KEY (booking_id) REFERENCES bookings(booking_id),
+  FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
