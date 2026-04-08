c68477adbea51e5bca52dff8686a9ecf66ca932e
suriyasivaprakashp <psspthe.12121971@gmail.com>
2026-04-08 17:45:17 +0530

    Update 01_Hotel_Schema_Setup.sql

--- SQL/01_Hotel_Schema_Setup.sql
+++ SQL/01_Hotel_Schema_Setup.sql
@@ -3,6 +3,11 @@ CREATE TABLE users (
   name VARCHAR(50)
 );
 
+INSERT INTO users VALUES
+(1, 'Ravi'),
+(2, 'Kumar'),
+(3, 'Arun');
+
 CREATE TABLE bookings (
   booking_id INT PRIMARY KEY,
   user_id INT,
@@ -11,11 +16,22 @@ CREATE TABLE bookings (
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+INSERT INTO bookings VALUES
+(101, 1, 201, '2021-11-10'),
+(102, 2, 202, '2021-11-15'),
+(103, 1, 203, '2021-12-01'),
+(104, 3, 204, '2021-12-05');
+
 CREATE TABLE items (
   item_id INT PRIMARY KEY,
   item_name VARCHAR(50),
   rate INT
 );
+
+INSERT INTO items VALUES
+(1, 'Food', 200),
+(2, 'Laundry', 100),
+(3, 'Spa', 500);
 
 CREATE TABLE booking_commercials (
   id INT PRIMARY KEY,
@@ -25,3 +41,10 @@ CREATE TABLE booking_commercials (
   FOREIGN KEY (booking_id) REFERENCES bookings(booking_id),
   FOREIGN KEY (item_id) REFERENCES items(item_id)
 );
+
+INSERT INTO booking_commercials VALUES
+(1, 101, 1, 2),  -- Food x2
+(2, 101, 2, 1),  -- Laundry x1
+(3, 102, 3, 1),  -- Spa x1
+(4, 103, 1, 3),  -- Food x3
+(5, 104, 2, 2);  -- Laundry x2
